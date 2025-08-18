65a4603b91d3c7a72b9ff119bbd2d55fda351b4c
coderV1 <pawarvikas534@gmail.com>
2025-08-18 18:51:35 +0530

    added buttons for Resume and Github

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion";
+import { TbFileCv } from "react-icons/tb";
+import { RiPagesFill } from "react-icons/ri";
+import { FaGithubSquare } from "react-icons/fa";
 import { allPortfolioList } from "../Constants/data";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Element } from "react-scroll";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,6 +14,8 @@ import ProjectCard from "./ProjectCard";
 import SectionHeader from "./SectionHeader";
 import Contact from "../Pages/Contact";
 import About from "../Pages/About";
+import { MyContext } from "../Context/ContextProvider";
+import { Link } from "react-router-dom";
 
 const getSize = () => {
   const windowWidth = window.innerWidth;
@@ -26,6 +31,7 @@ const getSize = () => {
 
 const Portfolio = () => {
   const [screen, setScreen] = useState(getSize());
+  const { handleToolTip } = useContext(MyContext);
 
   const settings = {
     dots: true,
@@ -33,10 +39,10 @@ const Portfolio = () => {
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
-    speed:2000,
+    speed: 2000,
     autoplaySpeed: 4000,
     easing: "ease",
-    pauseOnHover:true,
+    pauseOnHover: true,
     responsive: [
       {
         breakpoint: 1024,
@@ -44,25 +50,25 @@ const Portfolio = () => {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   useEffect(() => {
@@ -76,28 +82,28 @@ const Portfolio = () => {
       <Element name="home" className="element pt-16 w-full ">
         <div className=" py-4 bg-[url('/wave-haikei.svg')]  bg-cover bg-center lg:flex-row flex-col  flex w-full   justify-around">
           <div className="lg:w-full  flex">
-                <motion.div
-            initial={{ opacity: 0, translateX: "-40px" }}
-            whileInView={{ opacity: 1, translateX: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 1, ease: "anticipate", delay: 0.5 }}
-            className="lg:my-auto w-full  bg-white/15 dark:bg-black/15 overflow-hidden  p-4 rounded-xl backdrop-blur-md border-violet-400 my-auto text-left font-bold mx-4 border "
-          >
             <motion.div
-              initial={{
-                translateY: "200px",
-              }}
-              whileInView={{ translateY: 0 }}
-              transition={{ duration: 0.5, delay: 1 }}
+              initial={{ opacity: 0, translateX: "-40px" }}
+              whileInView={{ opacity: 1, translateX: 0 }}
               viewport={{ once: true }}
-              className="m-auto  py-4 "
+              transition={{ duration: 1, ease: "anticipate", delay: 0.5 }}
+              className="lg:my-auto w-full  bg-white/15 dark:bg-black/15 overflow-hidden  p-4 rounded-xl backdrop-blur-md border-violet-400 my-auto text-left font-bold mx-4 border "
             >
-              <p className="text-lg ">Hello I am </p>
-              <h1 className="text-6xl text-cyan-400 ">
-                Vikas <br /> Pawar
-              </h1>
-              <h3
-                className="
+              <motion.div
+                initial={{
+                  translateY: "200px",
+                }}
+                whileInView={{ translateY: 0 }}
+                transition={{ duration: 0.5, delay: 1 }}
+                viewport={{ once: true }}
+                className="m-auto  py-4 "
+              >
+                <p className="text-lg ">Hello I am </p>
+                <h1 className="text-6xl text-cyan-400 ">
+                  Vikas <br /> Pawar
+                </h1>
+                <h3
+                  className="
               text-4xl
               text-transparent 
               bg-clip-text 
@@ -106,24 +112,70 @@ const Portfolio = () => {
               from-orange-400 
               to-purple-500
               line-clamp-1"
+                >
+                  <Typewriter
+                    options={{
+                      strings: [
+                        "Full-Stack Web Developer",
+                        "Frontend Developer",
+                        "Backend Web Developer",
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 40,
+                      delay: 40,
+                    }}
+                  />
+                </h3>
+                <motion.div
+                 
+                  className=" py-4  px-2 my-1 sm:space-x-3 sm:w-fit  flex justify-evenly  sm:absolute top-0 right-0"
+                >
+                       <Link
+                         
+                          to={'https://drive.google.com/file/d/1Fiu2438wDy5NphBfqgbqH77G9mio1ARW/view?pli=1'}
+                          target="_blank"
+                          
+                        > <motion.button
+                   onMouseLeave={() => handleToolTip("")}
+                   onMouseEnter={() => handleToolTip(`Resume`)}
+                    initial={{
+                      translateX: "100px",
+                      opacity: 0,
+                    }}
+                    whileInView={{ translateX: 0, opacity: 1 }}
+                    transition={{ type: "spring", delay: 1.4 }}
+                    viewport={{ once: true }}
+                    className="border   mr-2  rounded-lg p-2"
+                  >
+                    <RiPagesFill className="hover:scale-125 transition-transform duration-150" size={64} />
+                    <p className="sm:hidden">Resume</p>
+                  </motion.button>
+                  </Link>
+                  <Link
+                         
+                          to={'https://github.com/vikasspawar'}
+                          target="_blank"
+                         
+                        >   <motion.button
+                    onMouseLeave={() => handleToolTip("")}
+                    onMouseEnter={() => handleToolTip(`Github`)}
+                    initial={{
+                      translateX: "100px",
+                      opacity: 0,
+                    }}
+                    whileInView={{ translateX: 0, opacity: 1  }}
+                    transition={{ type: "spring", delay: 1.5 }}
+                    viewport={{ once: true }}
+                    className="border  rounded-lg  p-2"
+                  >
+                    <FaGithubSquare className="hover:scale-125 transition-transform duration-150" size={64} />
+                 <p className="sm:hidden">Github</p> </motion.button>
+                 </Link>
               
-              >
-                <Typewriter
-                  options={{
-                    strings: [
-                      "Full-Stack Web Developer",
-                      "Frontend Developer",
-                      "Backend Web Developer",
-                    ],
-                    autoStart: true,
-                    loop: true,
-                    deleteSpeed: 40,
-                    delay: 40,
-                  }}
-                />
-              </h3>
+                </motion.div>
+              </motion.div>
             </motion.div>
-          </motion.div>
           </div>
           <motion.div
             initial={{ opacity: 0, translateX: "40px" }}
@@ -146,20 +198,16 @@ const Portfolio = () => {
       </Element>
 
       <Element name="about" className="element">
-        
-    <About/>
+        <About />
       </Element>
 
       <Element name="skills" className="elements  mb-12">
         <SectionHeader>Skills</SectionHeader>
         <Skills />
-      
       </Element>
 
       <Element name="projects" className="element mt-12">
-        <SectionHeader
-        headerColor="[rgb(186,104,200)]"
-        >Projects</SectionHeader>
+        <SectionHeader headerColor="[rgb(186,104,200)]">Projects</SectionHeader>
         <section className="mb-16 p-6 justify-center  ">
           <div className="">
             <div>
@@ -169,21 +217,19 @@ const Portfolio = () => {
                   {...settings}
                   slidesToShow={screen == "sm" ? 1 : screen == "md" ? 2 : 3}
                 > */}
-                  { 
-                    allPortfolioList.map((el,i)=>{
-                      return (
-                        <ProjectCard
-                          key={i}
-                          images={el.img}
-                          title={el.title}
-                          description={el.description}
-                          link={el.link}
-                          git={el.git_repo}
-                          subtitle={el.subtitle}
-                        />
-                      )
-                    })
-                  }
+                {allPortfolioList.map((el, i) => {
+                  return (
+                    <ProjectCard
+                      key={i}
+                      images={el.img}
+                      title={el.title}
+                      description={el.description}
+                      link={el.link}
+                      git={el.git_repo}
+                      subtitle={el.subtitle}
+                    />
+                  );
+                })}
                 {/* </Slider> */}
               </div>
             </div>
@@ -192,9 +238,9 @@ const Portfolio = () => {
       </Element>
 
       <Element name="contact" className="element">
-        <SectionHeader
-        headerColor={'[rgb(186,104,200)]'}
-        >Contact Me</SectionHeader>
+        <SectionHeader headerColor={"[rgb(186,104,200)]"}>
+          Contact Me
+        </SectionHeader>
         <Contact />
       </Element>
     </div>
